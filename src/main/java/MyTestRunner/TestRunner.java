74813b6546dc7c270a176af66239ba0d257b1b38
Erik Verna <vernaerik@gmail.com>
2024-05-23 18:20:48 -0300

    Fixed the hardcoded features path.

--- src/main/java/MyTestRunner/TestRunner.java
+++ src/main/java/MyTestRunner/TestRunner.java
@@ -6,9 +6,9 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions (
-        features = "C:\\Programacion\\Prueba\\cucumberTest\\src\\main\\java\\Features", //the path of the feature files
-        glue = {"stepDefinitions"},// the path of the step definition files
-        format = {"pretty", "html:test-outout"}
+        features = "src\\main\\java\\Features", //the path of the feature files
+        glue = {"stepDefinitions"}// the path of the step definition files
+//        monochrome = true
 )
 
 public class TestRunner {
